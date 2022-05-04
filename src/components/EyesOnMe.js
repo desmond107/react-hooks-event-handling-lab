88bd919dd7941c93eacb9d805f7b7646f8da36c3
desmond <desmond@desmonds-MacBook-Pro.local>
2022-05-04 12:27:00 +0300

    task complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+
+	function handlerBlur() {
+		console.log('Hey! Eyes on me!');
+	}
+
+	function handlerFocus() {
+		console.log('Good!');
+	}
+
+	return (
+		<button onBlur={handlerBlur} onFocus={handlerFocus}>Eyes on me</button>
+	);
+}
+
+export default EyesOnMe;
